4aa434c7b82f374b9f4c402e0c6712fb47fb2689
SabrinaPVaz <88401720+SabrinaPVaz@users.noreply.github.com>
2021-09-02 10:55:36 -0300

    Add files via upload

---
+++ OperationsMathSimple.py
@@ -0,0 +1,8 @@
+a = float(input("a: "))# input a float value for variable a here
+b = float(input("b: "))# input a float value for variable b here
+soma = a+b             # output the result of addition here
+subtrai = a-b          # output the result of subtraction here
+multiplica = a*b       # output the result of multiplication here
+divide = a/b           # output the result of division here
+print(soma,subtrai,multiplica,divide)
+print("\nThat's all, folks!")
